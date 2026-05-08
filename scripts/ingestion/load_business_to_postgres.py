3f4774ac6a05dc995de0192c1a1eadc5302f68f7
Paul Obrocta <obroctap@gmail.com>
2026-05-08 16:44:36 -0400

    ave current project updates

--- scripts/ingestion/load_business_to_postgres.py
+++ scripts/ingestion/load_business_to_postgres.py
@@ -2,25 +2,25 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 DB_USER = "postgres"
-DB_PASSWORD = "PaulObro!"
 DB_PASSWORD = "PaulObro"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "local_market_pipeline"
 
-csv_path = "data/raw/census/2026-05-06/census_demographics.csv"
+csv_path = "data/raw/business/zip_business_patterns.csv"
 
 engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
-df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path, dtype={"zip": str, "naics": str})
 
 df.to_sql(
-    "raw_census_demographics",
+    "zip_business_patterns_raw",
     engine,
     if_exists="replace",
     index=False
 )
 
-print("Loaded Census data into PostgreSQL table: raw_census_demographics")
+print("Loaded business data into PostgreSQL table: zip_business_patterns_raw")
+print(df.head())
